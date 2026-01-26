98d7d240ffe31f5a6a15433970eb2482ac915156
ANIL GOPAL SUTHAR <sutharani738@gmail.com>
2026-01-26 20:06:47 +0530

    Fix build error Explicitly pass Clerk key

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" className="dark scroll-smooth" suppressHydrationWarning>
         <body
           className={cn(
