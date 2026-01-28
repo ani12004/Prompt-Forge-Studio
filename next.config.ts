3581a1e4a0e1b5b1f4acdded3557eb477d1a791f
ANIL GOPAL SUTHAR <sutharani738@gmail.com>
2026-01-28 23:36:19 +0530

    fix: redirect profile to settings and update middleware

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/profile',
+        destination: '/settings',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
